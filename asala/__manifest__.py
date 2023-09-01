16da07f7395f4b4f8e53910ef533f5d775438dd3
NewAsala <74718174+NewAsala@users.noreply.github.com>
2023-09-01 14:34:11 +0300

    Add files via upload

---
+++ asala/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'asala',
+    'version': '1.1',
+    'category': 'asala',
+    'sequence': 1,
+    'summary': 'asala',
+    'website': 'https://www.odoo.com/app/asala',
+    'depends': [
+        'base_setup',
+        'web',
+    ],
+    'data': [
+        'views/asala.xml',
+    ],
+}
