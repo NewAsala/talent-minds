f516ed3b56ab5082df084a0eb81d92443c91025a
NewAsala <odoo@eupd04.odoo.com>
2023-09-01 17:53:37 +0000

    edit3

--- sp_website/__manifest__.py
+++ sp_website/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "My Company",
-    'website': "https://www.odoo.com/app/website/asala",
+    'website': "https://www.asala.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
