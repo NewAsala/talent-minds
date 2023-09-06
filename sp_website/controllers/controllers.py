5ba3a4a62fd3b36a106b5fc7b935092883d60fa4
NewAsala <odoo@eupd03.odoo.com>
2023-09-06 16:00:41 +0000

    edit

---
+++ sp_website/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+class SpWebsite(http.Controller):
+    @http.route('/', auth='public',website=True)
+    def index(self, **kw):
+        #return "Hello, world"
+        return http.request.render('sp_website.test_page',{})
+
+#     @http.route('/sp_website/sp_website/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('sp_website.listing', {
+#             'root': '/sp_website/sp_website',
+#             'objects': http.request.env['sp_website.sp_website'].search([]),
+#         })
+
+#     @http.route('/sp_website/sp_website/objects/<model("sp_website.sp_website"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('sp_website.object', {
+#             'object': obj
+#         })
