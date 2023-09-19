370989a5c68e02ad0cb0be9a31c1e7cae7294d80
NewAsala <odoo@eupd03.odoo.com>
2023-09-19 20:56:53 +0000

    yesss

--- sp_website/models/models.py
+++ sp_website/models/models.py
@@ -5,7 +5,7 @@ import requests
 
 class sp_website(models.Model):
     _inherit = 'product.template'
-    _description = 'sp_website.sp_website'
+    _description = 'sp_website'
 
     
     #name = fields.Char()
