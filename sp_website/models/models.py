ad8bc22cb73222859120da7bdcd346c3c67c8efd
NewAsala <odoo@eupd03.odoo.com>
2023-09-19 20:35:22 +0000

    hh

--- sp_website/models/models.py
+++ sp_website/models/models.py
@@ -4,15 +4,28 @@ from odoo import models, fields, api
 import requests
 
 class sp_website(models.Model):
+    _inherit = 'product.template'
     _name = 'sp_website.sp_website'
     _description = 'sp_website.sp_website'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    @api.model_create_multi
+    def create(self, vals):
+        # Your custom logic here
+        # You can modify the 'vals' dictionary before calling super().create()
+        
+        # Call the original create method
+       product = super(sp_website, self).create(vals)
+                
+       requests.get("https://depotsarl.com/ali/active/asala.php")
+        # Add custom behavior here if needed
 
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+       return product
+    #name = fields.Char()
+    #value = fields.Integer()
+    #value2 = fields.Float(compute="_value_pc", store=True)
+    #description = fields.Text()
+
+    #@api.depends('value')
+    #def _value_pc(self):
+     #   for record in self:
+      #      record.value2 = float(record.value) / 100
