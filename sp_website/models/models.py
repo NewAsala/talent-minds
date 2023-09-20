be4ea7fa2a5f2b7133931419309f5ad9bea7466b
NewAsala <odoo@eupd03.odoo.com>
2023-09-20 15:53:20 +0000

    edit60

--- sp_website/models/models.py
+++ sp_website/models/models.py
@@ -23,13 +23,13 @@ class sp_website(models.Model):
 
        return product
 
+    @api.model_create_multi
     def write(self, vals):
+        product = super(sp_website, self).create(vals)
 
-       product = super(sp_website, self).create(vals)
-
-       URL = "https://depotsarl.com/ali/active/asala.php"
-       PARAMS = {'action':'post_edit','id':product.id,'name':product.name}
-       requests.get(url = URL, params = PARAMS)
+        URL = "https://depotsarl.com/ali/active/asala.php"
+        PARAMS = {'action':'post_edit','id':product.id,'name':product.name}
+        requests.get(url = URL, params = PARAMS)
 
         return product
     #name = fields.Char()
