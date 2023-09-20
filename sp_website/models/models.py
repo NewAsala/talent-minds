57ea825ad0bf6554367f086364b95d2681625627
NewAsala <odoo@eupd03.odoo.com>
2023-09-20 16:41:36 +0000

    edit601

--- sp_website/models/models.py
+++ sp_website/models/models.py
@@ -16,7 +16,7 @@ class sp_website(models.Model):
        product = super(sp_website, self).create(vals)
 
        URL = "https://depotsarl.com/ali/active/asala.php"
-       PARAMS = {'action':'post_add','id':product.id,'name':product.name}
+       PARAMS = {'action':'post_add','id':product.id}
        requests.get(url = URL, params = PARAMS)
        #requests.get("https://depotsarl.com/ecomerce/odoo/api.php?action=post_edit&id="product.id)
         # Add custom behavior here if needed
@@ -28,7 +28,7 @@ class sp_website(models.Model):
         product = super(sp_website, self).create(vals)
 
         URL = "https://depotsarl.com/ali/active/asala.php"
-        PARAMS = {'action':'post_edit','id':product.id,'name':product.name}
+        PARAMS = {'action':'post_edit','id':product.id}
         requests.get(url = URL, params = PARAMS)
 
         return product
